ac7f85ae88b5ec9550d0de8e99044544ed8cd8d6
Mel Grosser <grosser.mel@live.de>
2021-06-04 16:22:34 +0200

    added css variables

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,15 +1,29 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+
+:root {
+    --light-blue: #86B3D1;
+    --dark-blue: #4D6D9A;
+    --mint: #99CED3;
+    --black: #5F6366;
+    --akzent-pink: #EDB5BF;
+    --white: #F0EBF4;
+}
+
 * {
   box-sizing: border-box;
   padding: 0;
   margin: 0;
 }
+
 body {
   font-family: sans-serif;
   font-size: 112.5%;
+  background: var(--white);
+  color: var(--black);
 }
+
 input,
 button,
 textarea {
