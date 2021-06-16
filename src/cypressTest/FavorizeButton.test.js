f3e337250cbc6a8b785a734efc52bb0483ae1f69
Mel Grosser <grosser.mel@live.de>
2021-06-16 10:37:10 +0200

    added cypress

---
+++ src/cypressTest/FavorizeButton.test.js
@@ -0,0 +1,19 @@
+/* eslint-disable jest/valid-expect-in-promise */
+/* eslint-disable no-undef */
+import React from 'react'
+import { mount } from '@cypress/react'
+import FavorizeButton from './FavorizeButton'
+import { RecoilRoot } from 'recoil'
+
+it('renders a text', () => {
+  mount(
+    <RecoilRoot>
+      <FavorizeButton id={666} />
+    </RecoilRoot>
+  )
+
+  cy.get('button').should('have.prop', 'click')
+  cy.get('button').should('be.visible')
+  cy.get('button').contains('Add to Favorites')
+  cy.get('button').should('contain.html', 'svg')
+})
