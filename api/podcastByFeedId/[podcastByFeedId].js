42b5609ae93f11d384a85ce70816d88359854d9d
Mel Grosser <grosser.mel@live.de>
2021-06-17 15:25:08 +0200

    ✨ added new hook to search a podcast by its feed

---
+++ api/podcastByFeedId/[podcastByFeedId].js
@@ -0,0 +1,10 @@
+import fetchDataFromDB from '../fetchDataFromDB.js'
+
+export default async function podcastByFeedId(req, res) {
+  const feedId = req.query.podcastByFeedId
+  const url = '/podcasts/byfeedid?id=' + feedId
+
+  const podcastByFeedId = await fetchDataFromDB(url)
+
+  res.status(200).json(podcastByFeedId)
+}
