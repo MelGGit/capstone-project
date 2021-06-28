8ac6f1158a18b3abaec215e5be50a56b92a7b395
Mel Grosser <grosser.mel@live.de>
2021-06-28 14:04:32 +0200

    revert-test

--- src/components/AudioPlayer/AudioPlayer.js
+++ src/components/AudioPlayer/AudioPlayer.js
@@ -11,6 +11,7 @@ AudioPlayer.propTyped = {
 export default function AudioPlayer({ audioObject }) {
   const [progress, setProgress] = useState(0)
   const [isPlaying, setIsPlaying] = useState(false)
+  const [isReady, setIsReady] = useState(false)
 
   const { image, title, author, audioSrc } = audioObject
   const audioRef = useRef(new Audio(audioSrc))
@@ -22,7 +23,14 @@ export default function AudioPlayer({ audioObject }) {
   useEffect(() => {
     audioRef.current = new Audio(audioSrc)
     setProgress(audioRef.current.currentTime)
-  }, [audioSrc])
+
+    if (isReady) {
+      audioRef.current.play()
+      setIsPlaying(true)
+      startTimer()
+    }
+    if (!isReady) setIsReady(true)
+  }, [isReady, audioSrc])
 
   // handle changes when clicking play and pause button
   useEffect(() => {
